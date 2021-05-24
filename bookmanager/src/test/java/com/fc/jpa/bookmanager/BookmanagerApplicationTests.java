0d331abb002ce1f521e3dac3c56acf9929f3b33e
k-gn <rlarbska97@gmail.com>
2021-05-24 12:03:58 +0900

    add jpa study

---
+++ bookmanager/src/test/java/com/fc/jpa/bookmanager/BookmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fc.jpa.bookmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
