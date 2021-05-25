a2c511f3919b4b2392adbe33fdcf18ec8a931feb
k-gn <rlarbska97@gmail.com>
2021-05-26 00:08:38 +0900

    security study

--- springboot-fc-security/sp-fastcampus-spring-sec-4-1-login-custom-filter/server/login-custom-filter/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ springboot-fc-security/sp-fastcampus-spring-sec-4-1-login-custom-filter/server/login-custom-filter/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.sp.fc.web.config;
 
+import com.sp.fc.web.student.StudentManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,13 +12,30 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity(debug = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final StudentManager manager;
+
+    public SecurityConfig(StudentManager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(manager);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(request->
-                        request.anyRequest().permitAll()
+                        request
+                                .antMatchers("/").permitAll()
+                                .anyRequest().authenticated()
                 )
+                .formLogin(
+                        login -> login.loginPage("/login")
+                        .permitAll()
+                )
+                .csrf().disable()
                 ;
     }
 
