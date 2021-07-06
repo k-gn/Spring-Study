0f20322c639de96a5f4b7d3c65e95da863a3ad93
k-gn <rlarbska97@gmail.com>
2021-07-06 12:48:16 +0900

    no message

---
+++ JavaAlgo/src/algo/algo04.java
@@ -0,0 +1,14 @@
+package algo;
+
+import java.util.Scanner;
+
+public class algo04 {
+
+    public static void main(String[] args) {
+
+        Scanner stdIn = new Scanner(System.in);
+        int num = stdIn.nextInt();
+        System.out.println(num);
+    }
+
+}
