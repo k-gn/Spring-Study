25d1b7c88d34c7f965e65b1d3c680e0ccae6493a
k-gn <rlarbska97@gmail.com>
2021-07-06 12:58:44 +0900

    no message

--- JavaAlgo/src/algo/algo04.java
+++ JavaAlgo/src/algo/algo04.java
@@ -2,13 +2,25 @@ package algo;
 
 import java.util.Scanner;
 
+// 중앙값 구하기
 public class algo04 {
+
+    public static int mid(int a, int b, int c) {
+        // 1 2 3
+        if((a < b && a > c) || (a > b && a < c)) {
+            return a;
+        }else if((b < a && b > c) || (b > a && b < c)) {
+            return b;
+        }else {
+            return c;
+        }
+    }
 
     public static void main(String[] args) {
 
-        Scanner stdIn = new Scanner(System.in);
-        int num = stdIn.nextInt();
-        System.out.println(num);
+        int a = 1, b = 4, c = 8;
+        int result = mid(a, b, c);
+        System.out.println("mid : " + result);
     }
 
 }
