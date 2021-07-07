c6d3fc54410292eee3ce9b60f041b1076ef4e152
k-gn <rlarbska97@gmail.com>
2021-07-07 14:35:30 +0900

    no message

---
+++ JavaAlgo/src/algo/algo11.java
@@ -0,0 +1,47 @@
+package algo;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class algo11 {
+
+    public static boolean equalArr(int[] a, int[] b) {
+        if(a.length != b.length) {
+            return false;
+        }
+
+        for(int i=0; i<a.length; i++) {
+            if(a[i] != b[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void rcopy(int[] a, int[] b) {
+        b = a.clone();
+        for(int i=0; i<b.length/2; i++) {
+            int temp = b[i];
+            b[i] = b[b.length - 1 - i];
+            b[b.length - 1 - i] = temp;
+        }
+        System.out.println(Arrays.toString(b));
+    }
+
+    public static void main(String[] args) throws IOException {
+
+//       BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//       String s = bf.readLine();
+//       System.out.println(s);
+        int[] num1 = {1, 2, 3, 4, 5};
+        int[] num2 = {1, 2, 3, 4, 6};
+        int[] num3 = null;
+
+        rcopy(num1, num3);
+
+        boolean check = equalArr(num1, num2);
+        System.out.println(check ? "두 배열은 같습니다" : "두 배열은 같지 않습니다.");
+    }
+
+}
---
+++ JavaAlgo/src/algo/algo12.java
@@ -0,0 +1,38 @@
+package algo;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class algo12 {
+
+    public static void main(String[] args) throws IOException {
+
+        int i = 127;
+        int num = 8;
+        char[] index = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        char[] arr = new char[32];
+        int digit = 0;
+        while(i > 0) {
+            int temp = i % num;
+            i = i / num;
+            arr[digit++] = index[temp];
+        }
+        
+        for(int j=0; j<digit; j++) {
+            char temp = arr[j];
+            arr[j] = arr[arr.length - 1 - j];
+            arr[arr.length - 1 - j] = temp;
+            System.out.println(arr);
+        }
+
+        for(char c : arr) {
+            if(c != '\u0000') {
+                System.out.print(c);
+            }
+        }
+
+//        String str = Integer.toString(i, 16);
+//        System.out.println(str);
+    }
+
+}
