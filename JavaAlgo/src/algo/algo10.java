1e0d903be1505e07899949082df7cbb93664af6c
k-gn <rlarbska97@gmail.com>
2021-07-07 13:06:38 +0900

    no message

---
+++ JavaAlgo/src/algo/algo10.java
@@ -0,0 +1,35 @@
+package algo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class algo10 {
+
+    public static void main(String[] args) throws IOException {
+
+//       BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//       String s = bf.readLine();
+//       System.out.println(s);
+        int[] num = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int a : num) {
+            sum += a;
+        }
+        System.out.println("요소의 합 : " + sum);
+        System.out.println(Arrays.toString(num));
+        for (int i = 0; i < num.length / 2; i++) {
+            System.out.println(num[i] + " 와 " + num[num.length - 1 - i] + " 의 자리를 바꿉니다.");
+            int temp = num[i];
+            num[i] = num[num.length - 1 - i];
+            num[num.length - 1 - i] = temp;
+            System.out.println(Arrays.toString(num));
+        }
+
+    }
+
+}
