3a3158b3eb08fd8fb09999192ea038d19c0886cf
k-gn <rlarbska97@gmail.com>
2021-07-08 12:46:02 +0900

    no message

--- JavaAlgo/src/clazz/PhysicalExamination.java
+++ JavaAlgo/src/clazz/PhysicalExamination.java
@@ -1,7 +1,9 @@
 package clazz;
 
-public class PhysicalExamination {
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
+public class PhysicalExamination {
 
 
 }
@@ -20,7 +22,7 @@ class PhyscData {
     static double aveHeight(PhyscData[] dat) {
         double sum = 0;
 
-        for(int i=0; i<dat.length; i++) {
+        for (int i = 0; i < dat.length; i++) {
             sum += dat[i].height;
         }
 
@@ -28,15 +30,39 @@ class PhyscData {
     }
 
     static void distVision(PhyscData[] dat, int[] dist) {
-       int i = 0;
+        int i = 0;
 
-       dist[i] = 0;
-
-        for(i=0; i<dat.length; i++) {
-            if(dat[i].vision >= 0.0 && dat[i].vision <= 2.0) {
-                dist[(int)dat[i].vision * 10]++;
+        for (i = 0; i < dat.length; i++) {
+            if (dat[i].vision >= 0.0 && dat[i].vision <= 2.0) {
+                dist[(int) (dat[i].vision * 10)]++;
             }
         }
 
+    }
+
+    public static void main(String[] args) {
+        PhyscData[] x = {
+                new PhyscData("이나령", 162, 0.3),
+                new PhyscData("전서현", 173, 0.7),
+                new PhyscData("이수민", 175, 2.0),
+                new PhyscData("홍준기", 171, 1.5),
+                new PhyscData("유지훈", 168, 1.2),
+                new PhyscData("이호연", 174, 1.2),
+                new PhyscData("김한결", 169, 0.8),};
+
+        int[] vdist = new int[21];
+        DecimalFormat format = new DecimalFormat("0.0");
+        System.out.println(" 평균 키 : " + format.format(aveHeight(x)));
+
+        distVision(x, vdist);
+        for(int i=0; i<21; i++) {
+
+//            System.out.printf("%.1f ~ : %d명\n", i/10.0, vdist[i]);
+            System.out.printf("%.1f ~ : ", i/10.0);
+            for(int j=0; j<vdist[i]; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
---
+++ JavaAlgo/src/clazz/YMD.java
@@ -0,0 +1,114 @@
+package clazz;
+
+public class YMD {
+
+    private int y = 2021;
+    private int m = 12;
+    private int d = 30;
+
+    static int[][] mdays = {{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}, // 평년
+            {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}, // 윤년
+    };
+
+    static int isLeap(int year) {
+        return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) ? 1 : 0;
+    }
+
+    public YMD() {}
+
+    public YMD(int y, int m, int d) {
+        this.y = y;
+        this.m = m;
+        this.d = d;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getM() {
+        return m;
+    }
+
+    public void setM(int m) {
+        this.m = m;
+    }
+
+    public int getD() {
+        return d;
+    }
+
+    public void setD(int d) {
+        if(d <= 0) {
+            d = 1;
+        }
+        this.d = d;
+    }
+
+    YMD after(int n) {
+
+        YMD ymd = new YMD(this.y, this.m, this.d);
+
+        int nowDay = ymd.getD();
+        int monthDay = mdays[isLeap(y)][m - 1];
+        int afterDay = nowDay + n;
+        System.out.println("afterDay : " + afterDay);
+        System.out.println("monthDay : " + monthDay);
+        System.out.println("nowDay : " + nowDay);
+        if(afterDay > monthDay) {
+            ymd.setD(afterDay - monthDay);
+            if(ymd.getM() == 12) {
+                ymd.setY(ymd.getY() + 1);
+                ymd.setM(1);
+            }else {
+                ymd.setM(ymd.getM() + 1);
+            }
+        }else {
+            ymd.setD(afterDay);
+        }
+
+        return ymd;
+    }
+
+    YMD before(int n) {
+
+        YMD ymd = new YMD(this.y, this.m, this.d);
+
+        int nowDay = ymd.getD();
+        int beformMonthDay = mdays[isLeap(y)][m - 2 <= 0 ? 12 + (m-2) : m-2];
+        int beforeDay = nowDay - n;
+        if(beforeDay <= 0) {
+            ymd.setD(beformMonthDay - (n - nowDay));
+            if(ymd.getM() == 1) {
+                ymd.setY(ymd.getY() - 1);
+                ymd.setM(12);
+            }else {
+                ymd.setM(ymd.getM() - 1);
+            }
+        }else {
+            ymd.setD(beforeDay);
+        }
+
+        return ymd;
+    }
+
+    public static void main(String[] args) {
+
+        YMD ymd = new YMD();
+        System.out.println("after : " + ymd.after(3));
+//        System.out.println("before : " + ymd.before(3));
+    }
+
+    @Override
+    public String toString() {
+        return "YMD : " +
+                "y = " + y +
+                ", m = " + m +
+                ", d = " + d;
+
+    }
+}
