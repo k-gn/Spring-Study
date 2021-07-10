38e57dc7dbc05d8ed8a01bf6cf487edbf1f2376b
k-gn <rlarbska97@gmail.com>
2021-07-10 13:22:07 +0900

    no message

--- JavaAlgo/src/clazz/MyQueue.java
+++ JavaAlgo/src/clazz/MyQueue.java
@@ -1,4 +1,73 @@
 package clazz;
 
-public class MyQueue {
+interface IQueue {
+    public void enQueue(String data);
+
+    public String deQueue();
+
+    public void printAll();
+}
+
+
+public class MyQueue extends MyLinkedList implements IQueue {
+
+//    private int front;
+//    private int rear;
+
+    MyListNode front;
+    MyListNode rear;
+
+
+    public MyQueue() {
+        front = null;
+        rear = null;
+    }
+
+
+    @Override
+    public void enQueue(String data) {
+
+        MyListNode newNode;
+        if(isEmpty()) {
+            newNode = addElement(data);
+            front = newNode;
+            rear = newNode;
+        }else {
+            newNode = addElement(data);
+            rear = newNode;
+        }
+        System.out.println(newNode.getData() + " added");
+    }
+
+    @Override
+    public String deQueue() {
+        if(isEmpty()){
+            System.out.println("Queue is Empty");
+            return null;
+        }
+
+        String data = front.getData();
+        front = front.next;
+        if(front == null) {
+            rear = null;
+        }
+
+        return data;
+    }
+
+    @Override
+    public void printAll() {
+        if(isEmpty()){
+            System.out.println("Queue is Empty");
+            return;
+        }
+
+        MyListNode temp = front;
+        while(temp!= null){
+            System.out.print(temp.getData() + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+
+    }
 }
