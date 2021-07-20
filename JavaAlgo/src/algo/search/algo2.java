5fdb2210972fde919a7bc6ebf734854fdb0c9e67
k-gn <rlarbska97@gmail.com>
2021-07-20 12:19:17 +0900

    no message

--- JavaAlgo/src/algo/search/algo2.java
+++ JavaAlgo/src/algo/search/algo2.java
@@ -1,13 +1,24 @@
 package algo.search;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class algo2 {
 
     public static void main(String[] args) {
 
 
-        int[] arr = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+        int[] arr = {1, 3, 3, 3, 3, 3, 13, 15, 17, 19};
+        List<Integer> integers = new ArrayList<>();
+
+        integers.add(1);
+        integers.add(2);
+        integers.add(2);
+        integers.add(2);
+        integers.add(3);
+        integers.add(4);
 
         int key = 16;
         int left = 0;
@@ -48,8 +59,9 @@ public class algo2 {
             System.out.println("해당 값이 존재하지 않습니다.");
         }
 
-        int idx = Arrays.binarySearch(arr, 17);
-        System.out.println("idx : " + idx);
+        int idx1 = Arrays.binarySearch(arr, 0, arr.length, 3);
+//        System.out.println("idx : " + idx1);
+
 
     }
 }
