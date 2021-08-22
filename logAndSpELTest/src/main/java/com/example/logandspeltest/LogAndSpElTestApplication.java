215250806bff31eb7307153d9a2bf7fd4f474b98
k-gn <rlarbska97@gmail.com>
2021-08-22 13:24:28 +0900

    no message

---
+++ logAndSpELTest/src/main/java/com/example/logandspeltest/LogAndSpElTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.logandspeltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAndSpElTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogAndSpElTestApplication.class, args);
+    }
+
+}
