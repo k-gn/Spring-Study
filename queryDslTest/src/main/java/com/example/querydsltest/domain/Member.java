72f89697d134ae6662fe7d2f15080d2ad6209910
k-gn <rlarbska97@gmail.com>
2021-08-22 11:48:13 +0900

    no message

---
+++ queryDslTest/src/main/java/com/example/querydsltest/domain/Member.java
@@ -0,0 +1,44 @@
+package com.example.querydsltest.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+// 기본 생성자의 접근 제어를 PROTECTED로 설정해놓게 되면 무분별한 객체 생성에 대해 한번 더 체크할 수 있는 수단이 되기 때문
+// 객체 생성 시 안전성을 어느 정도 보장
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String username;
+
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username) {
+        this(username, 0);
+    }
+
+    public Member(String username, int age) {
+        this(username, age, null);
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null) {
+            this.team = team;
+        }
+    }
+}
--- queryDslTest/src/test/java/com/example/querydsltest/QueryDslTestApplicationTests.java
+++ queryDslTest/src/test/java/com/example/querydsltest/QueryDslTestApplicationTests.java
@@ -1,13 +1,72 @@
 package com.example.querydsltest;
 
+import com.example.querydsltest.domain.*;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class QueryDslTestApplicationTests {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory query;
+
+    @BeforeEach
+    void init() {
+        query = new JPAQueryFactory(em);
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
 
     @Test
     void contextLoads() {
+
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        QHello qHello = QHello.hello; // Querydsl Q타입 객체
+
+        Hello result = query.selectFrom(qHello).fetchOne();
+        System.out.println(result);
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
+    @Test
+    void memberTest() {
+        QMember m = new QMember("m");
+        Member findMember = query
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1"))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
 }
